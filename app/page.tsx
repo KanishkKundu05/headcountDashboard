e8ebf292e2e79d0e09c93dd746c70e789761a933
Kanishk Kundu <kanishk.kundu@nyu.edu>
2025-12-05 01:39:44 -0500

    employees list in page.tsx & removed drop down

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { DataTableDemo } from "@/components/EmployeeList";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen font-sans dark:bg-black">
+        <DataTableDemo />
     </div>
   );
 }
